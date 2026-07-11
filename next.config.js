779973e5e6d6a634569752fdf6d960f214fa1327
Hakkim dell <hakkim0948@gmail.com>
2026-07-11 13:27:57 +0530

    Deploy portfolio to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  basePath: isProd ? '/Portfolio' : '',
+  assetPrefix: isProd ? '/Portfolio/' : '',
+  trailingSlash: true,
 };
-
 module.exports = nextConfig;
